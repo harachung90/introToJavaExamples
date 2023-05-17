be9ed1275dc2f21cc97ac0c2a40023652c7e50c2
Hara <harachung90@gmail.com>
2023-05-17 20:19:07 +1000

    Second commit. Multiple strings, multiple format strings, errors, coding the prototype

--- Intro.java
+++ Intro.java
@@ -4,8 +4,13 @@ public class Intro {
     public static void main(String[] args) {
 
         Console console = System.console();
-        String firstName = console.readLine("What is your name? ");
-        console.printf("Hello, my name is %s\n", firstName);
-        console.printf("%s is learning how to write Java\n", firstName);
+        String name = console.readLine("What is your name? ");
+        String adjective = console.readLine("Enter an adjective: ");
+        String noun = console.readLine("Enter a noun: ");
+        String adverb = console.readLine("Enter an adjective: ");
+        String verb = console.readLine("Enter a verb ending in -ing: ");
+        console.printf("Your story:\n-------------------------\n");
+        console.printf("%s is a %s %s\n", name, adjective, noun);
+        console.printf("They are always %s %s.", name, adjective);
     }
 }
