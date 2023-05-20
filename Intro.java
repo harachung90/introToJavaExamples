d714799288b8e226d1d0cbc96df761a3cbd2126b
Hara <harachung90@gmail.com>
2023-05-20 19:17:54 +1000

    Fifth commit. Censoring words - using logical ORs, using logical ORs and ANDs, Censoring words - looping until value passes

--- Intro.java
+++ Intro.java
@@ -14,11 +14,17 @@ public class Intro {
 
         String name = console.readLine("Enter a name: ");
         String adjective = console.readLine("Enter an adjective: ");
-        String noun = console.readLine("Enter a noun: ");
-        if (noun.equalsIgnoreCase("dork")) {
-            console.printf("That language is not allowed. Exiting. \n\n");
-            System.exit(0);
-        }
+        String noun;
+        boolean isInvalidWord;
+
+        do {
+            noun = console.readLine("Enter a noun: ");
+            isInvalidWord = (noun.equalsIgnoreCase("dork") ||
+                    noun.equalsIgnoreCase("jerk"));
+            if (isInvalidWord) {
+                console.printf("That language is not allowed. Try again. \n\n");
+            }
+        } while (isInvalidWord);
 
         String adverb = console.readLine("Enter an adjective: ");
         String verb = console.readLine("Enter a verb ending in -ing: ");
